c698a8c551bad120b3ada6cfd5ff98db7771c92e
Haris <harishashim@gmail.com>
2019-03-01 08:38:28 +0800

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  root: true,
+
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+
+  env: {
+    browser: true
+  },
+
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+
+  globals: {
+    'ga': true, // Google Analytics
+    'cordova': true,
+    '__statics': true,
+    'process': true
+  },
+
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow paren-less arrow functions
+    'arrow-parens': 'off',
+    'one-var': 'off',
+
+    'import/first': 'off',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'prefer-promise-reject-errors': 'off',
+
+    // Custom config
+    "space-before-function-paren": [0, "never"],
+    semi: 0,
+    "brace-style": 0,
+    quotes: 0,
+    indent: 0,
+    "key-spacing": 0,
+    "no-tabs": 0,
+    "no-mixed-spaces-and-tabs": 0,
+    
+    // allow console.log during development only
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow debugger during development only
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,135 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- <q-header elevated class>
+      <q-toolbar style="height:55px;">
+        <q-img
+          src="/statics/snlogo.png"
+          spinner-color="white"
+          style="height: 47px; max-width: 264px"
+        />
+      </q-toolbar>
+    </q-header>-->
+    <div class style="width:100%;">
+      <q-carousel
+        v-model="slideName"
+        :height="windowHeight"
+        class
+        animated
+        arrows
+        navigation
+        autoplay
+      >
+        <q-carousel-slide :name="1" img-src="/statics/photo/analog.jpg"/>
+        <q-carousel-slide :name="2" img-src="/statics/photo/bright.jpg"/>
+        <q-carousel-slide :name="3" img-src="/statics/photo/application.jpg"/>
+        <q-carousel-slide :name="4" img-src="/statics/photo/board.jpg"/>
+      </q-carousel>
+
+      <q-card
+        :class="{'card-top-right':isOne, 'card-top-left':isTwo,'card-bottom-left':isThree,
+        'card-bottom-right':isFour, 'bg-secondary':true}"
+        dark
+        bordered
+        style="width: 100%; max-width: 200px; opacity:0.7;"
+      >
+        <q-card-section class="text-h6 item-center">
+          <div style="color:white; text-align: center;">{{slideText[slideName-1]}}</div>
+        </q-card-section>
+      </q-card>
+
+      <!-- <q-card
+        :class="{'card-bottom-left':isFour, 'bg-secondary':true}"
+        dark
+        bordered
+        style="width: 310px; height: 91px; opacity:.9;"
+      >
+      <q-card-section class="item-center">
+      :class="{'card-bottom-left':isFour}"-->
+      <q-img
+        v-if="isFour"
+        class="fixed-center q-mt-lg"
+        style="width: 310px; height: 91px; opacity:.9;"
+        src="/statics/snlogo.png"
+        spinner-color="white"
+      />
+      <!-- </q-card-section>
+      </q-card>-->
+    </div>
+  </q-page>
+</template>
+
+<style>
+.my-card {
+  width: "100%";
+  max-width: "250px;";
+}
+
+.card-top-right {
+  position: absolute;
+  margin-top: 64px;
+  margin-right: 64px;
+  padding: 16px;
+  right: 0;
+  top: 0;
+}
+
+.card-top-left {
+  position: absolute;
+  margin-top: 64px;
+  margin-left: 64px;
+  padding: 16px;
+  top: 0;
+  left: 0;
+}
+
+.card-bottom-left {
+  position: absolute;
+  margin-bottom: 64px;
+  margin-left: 64px;
+  padding: 16px;
+  bottom: 0;
+  left: 0;
+}
+.card-bottom-right {
+  position: absolute;
+  margin-bottom: 64px;
+  margin-right: 64px;
+  padding: 16px;
+  bottom: 0;
+  right: 0;
+}
+</style>
+
+<script>
+export default {
+  name: "PageIndex",
+  data() {
+    return {
+      slideName: 1,
+      slideText: [
+        "One is the loneliest number ...",
+        "Bright idea worthy of bright compensation",
+        "Excelence through design!",
+        "Stay tuned, we are coming!"
+      ]
+    };
+  },
+  created() {
+    this.windowHeight = window.innerHeight + "px";
+  },
+  computed: {
+    isOne() {
+      return this.slideName === 1;
+    },
+    isTwo() {
+      return this.slideName === 2;
+    },
+    isThree() {
+      return this.slideName === 3;
+    },
+    isFour() {
+      return this.slideName === 4;
+    }
+  }
+};
+</script>
